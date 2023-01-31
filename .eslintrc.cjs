7e960f7935ba4b053226a84a8c6b820b151e219b
jyjblrd <jyjbird@gmail.com>
2023-01-31 15:56:18 +0000

    boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FlowBuilder from './components/FlowBuilder';
+
+export default function App() {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <FlowBuilder />
+    </div>
+  );
+}
