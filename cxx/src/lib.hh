0c717061ffe4eb8b121f6fcf93561365a15782a7
Ben Stokes <bcs42@cam.ac.uk>
2023-02-09 11:17:09 +0000

    C++ refactoring

--- cxx/src/lib.hh
+++ cxx/src/lib.hh
@@ -22,16 +22,14 @@ struct Vertex {
 
 auto parse_kind(std::string_view const kind_str) -> std::optional<VertexKind> {
 	using std::operator""sv;
-	using std::make_optional;
-	using std::nullopt;
 
 	if (false) {
 	} else if (kind_str == "DigitalPinInPullDown"sv) {
-		return make_optional(VertexKind::DigitalPinInPullDown);
+		return {VertexKind::DigitalPinInPullDown};
 	} else if (kind_str == "Conjunction"sv) {
-		return make_optional(VertexKind::Conjunction);
+		return {VertexKind::Conjunction};
 	} else if (kind_str == "DigitalPinOut"sv) {
-		return make_optional(VertexKind::DigitalPinOut);
+		return {VertexKind::DigitalPinOut};
 	}
 
 	return {};
