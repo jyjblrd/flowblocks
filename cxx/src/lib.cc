c153e795e475a9e10471a50569914f4e2534ae02
Ben Stokes <bcs42@cam.ac.uk>
2023-02-13 02:20:20 +0000

    type and sanity checking

--- cxx/src/lib.cc
+++ cxx/src/lib.cc
@@ -16,14 +16,23 @@ auto compile(std::map<std::string, Node> const &vertices) -> std::string {
 	using std::tuple;
 	using std::vector;
 
+	if (vertices.empty()) {
+		return "# Empty graph"; // TODO: actual error handling
+	}
+
 	string code {"import machine\n"};
 
 	map<string, map<string, vector<Dependency>>> id_to_output_to_successor_id_input {};
 	map<string, vector<tuple<string, Node>>> successors {};
-	map<string, size_t> degree {};
+	map<string, std::size_t> degree {};
 
 	for (auto const &[id, vertex] : vertices) {
 		for (auto const &[input, predecessor_output] : vertex.predecessors) {
+			auto input_type = get_input_type(parse_node_type(vertex.node_type).value(), input);
+			auto output_type = get_output_type(parse_node_type(vertices.at(predecessor_output.id).node_type).value(), predecessor_output.handle);
+			if (!input_type || !output_type || *input_type != *output_type) {
+				return "# Type checking failed"; // TODO: actual error handling
+			}
 			id_to_output_to_successor_id_input[predecessor_output.id][predecessor_output.handle].push_back(Dependency {id, input});
 			successors[predecessor_output.id].emplace_back(id, vertex);
 			++degree[id];
@@ -38,11 +47,14 @@ auto compile(std::map<std::string, Node> const &vertices) -> std::string {
 			upcoming_vertices.emplace_back(id, vertex);
 			nonsuccessor_ids.push_back(id);
 		}
+		if (degree[id] != expected_indegree(parse_node_type(vertex.node_type).value())) {
+			return "# Missing input(s)"; // TODO: actual error handling
+		}
 	}
 
 	set<string> visited_ids {};
 	vector<tuple<string, Node>> sorted_vertices {};
-	set<string> node_types {};  // only for generating class definitions
+	set<string> node_types {}; // only for generating class definitions
 
 	while (!upcoming_vertices.empty()) {
 		auto const &[id, vertex] = upcoming_vertices.front();
@@ -64,14 +76,14 @@ auto compile(std::map<std::string, Node> const &vertices) -> std::string {
 	}
 
 	map<string, NodeType> parsed_node_types;
- 
+
 	for (auto const &node_type_str : node_types) {
 		if (auto const node_type = parse_node_type(node_type_str); node_type) {
-            parsed_node_types[node_type_str] = *node_type;
+			parsed_node_types[node_type_str] = *node_type;
 			code.append(block_class_definition(*node_type));
 		}
 	}
- 
+
 	for (auto it = sorted_vertices.rbegin(); it != sorted_vertices.rend(); ++it) {
 		auto const &[id, vertex] = *it;
 		code.append(block_initialization(parsed_node_types[vertex.node_type], id, id_to_output_to_successor_id_input[id]));
--- cxx/src/lib.hh
+++ cxx/src/lib.hh
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <map>
 #include <optional>
+#include <cstddef>
 
 struct Dependency {
 	std::string id;
@@ -20,17 +22,74 @@ struct Node {
 	std::map<std::string, Dependency> predecessors;
 };
 
+enum class ConnectionType {
+	Bool,
+	Number,
+};
+
 auto parse_node_type(std::string_view const kind_str) -> std::optional<NodeType> {
 	using std::operator""sv;
 
 	if (false) {
 	} else if (kind_str == "DigitalPinInPullDown"sv) {
-		return { NodeType::DigitalPinInPullDown};
+		return {NodeType::DigitalPinInPullDown};
 	} else if (kind_str == "Conjunction"sv) {
-		return { NodeType::Conjunction};
+		return {NodeType::Conjunction};
 	} else if (kind_str == "DigitalPinOut"sv) {
-		return { NodeType::DigitalPinOut};
+		return {NodeType::DigitalPinOut};
 	}
 
 	return {};
+}
+
+auto get_input_type(NodeType node_type, std::string_view input_id) -> std::optional<ConnectionType> {
+	using std::operator""sv;
+
+	switch (node_type) {
+	case NodeType::DigitalPinInPullDown:
+		break;
+	case NodeType::Conjunction:
+		if (input_id == "1"sv || input_id == "2"sv) {
+			return {ConnectionType::Bool};
+		}
+		break;
+	case NodeType::DigitalPinOut:
+		if (input_id == "1"sv) {
+			return {ConnectionType::Bool};
+		}
+		break;
+	}
+	return {};
+}
+
+auto get_output_type(NodeType node_type, std::string_view output_id) -> std::optional<ConnectionType> {
+	using std::operator""sv;
+
+	switch (node_type) {
+	case NodeType::DigitalPinInPullDown:
+		if (output_id == "1"sv) {
+			return {ConnectionType::Bool};
+		}
+		break;
+	case NodeType::Conjunction:
+		if (output_id == "1"sv) {
+			return {ConnectionType::Bool};
+		}
+		break;
+	case NodeType::DigitalPinOut:
+		break;
+	}
+	return {};
+}
+
+auto expected_indegree(NodeType node_type) -> std::size_t {
+	switch (node_type) {
+	case NodeType::DigitalPinInPullDown:
+		return 0;
+	case NodeType::Conjunction:
+		return 2;
+	case NodeType::DigitalPinOut:
+		return 1;
+	}
+	return 0;
 }
