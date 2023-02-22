d395f44157d793d2e91236401a52d4072ccb912e
jyjblrd <jyjbird@gmail.com>
2023-02-22 14:51:03 +0000

    fix type error

--- src/components/NodePicker.tsx
+++ src/components/NodePicker.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import { useRecoilValue } from 'recoil';
+import { NodeInstance } from '../shared/interfaces/NodeInstance.interface';
+import { NodeTypeData } from '../shared/interfaces/NodeTypes.interface';
 import { nodeTypesAtom } from '../shared/recoil/atoms/nodeTypesAtom';
 import DefaultNode from './DefaultNode';
 import Draggable from './Draggable';
+
+function genDummyNodeInstance(nodeTypeId: string, nodeType: NodeTypeData): NodeInstance {
+  return {
+    nodeTypeId,
+    connections: {},
+    attributes: {},
+    isInputConnected: Array<number>(Object.entries(nodeType.inputs).length).fill(0),
+    isOutputConnected: Array<number>(Object.entries(nodeType.outputs).length).fill(0),
+  };
+}
 
 export default function NodePicker() {
   const nodeTypes = useRecoilValue(nodeTypesAtom);
@@ -22,7 +34,7 @@ export default function NodePicker() {
                 <Draggable id={nodeTypeId} data={{ nodeTypeId }}>
                   <DefaultNode
                     isDummyNode
-                    data={{ ...nodeType, nodeTypeId }}
+                    data={genDummyNodeInstance(nodeTypeId, nodeType)}
                   />
                 </Draggable>
               </div>
