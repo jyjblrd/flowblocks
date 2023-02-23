134ade8679a816c79f0f1c22bd100fb043492cf3
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-02-23 00:25:48 +0000

    Make handle colors type-aware

--- src/components/DefaultNode.tsx
+++ src/components/DefaultNode.tsx
@@ -6,6 +6,7 @@ import {
 } from 'reactflow';
 import { useRecoilValue } from 'recoil';
 import { NodeInstance } from '../shared/interfaces/NodeInstance.interface';
+import { ConnectionType } from '../shared/interfaces/NodeTypes.interface';
 import { nodeTypesAtom } from '../shared/recoil/atoms/nodeTypesAtom';
 import './DefaultNode.scss';
 
@@ -46,6 +47,11 @@ export default function DefaultNode(
   const numInputs = Object.entries(nodeType.inputs).length;
   const numOutputs = Object.entries(nodeType.outputs).length;
 
+  const typeToColor: Record<ConnectionType, string> = {
+    [ConnectionType.Bool]: 'var(--bs-blue)',
+    [ConnectionType.Number]: 'var(--bs-orange)',
+  };
+
   return (
     <>
       {
@@ -65,7 +71,11 @@ export default function DefaultNode(
                 position={Position.Left}
                 id={key}
                 key={key}
-                style={{ top: calcHandleTop(index, numInputs) }}
+                style={{
+                  top: calcHandleTop(index, numInputs),
+                  backgroundColor: data.isInputConnected[index] ? typeToColor[nodeType.inputs[index].type] : 'var(--bs-white)',
+                  borderColor: typeToColor[nodeType.inputs[index].type],
+                }}
                 className={data.isInputConnected[index] ? 'connected' : ''}
                 isValidConnection={useConnectionValidator()}
               />
@@ -95,7 +105,11 @@ export default function DefaultNode(
                 position={Position.Right}
                 id={key}
                 key={key}
-                style={{ top: calcHandleTop(index, numOutputs) }}
+                style={{
+                  top: calcHandleTop(index, numOutputs),
+                  backgroundColor: data.isOutputConnected[index] ? typeToColor[nodeType.outputs[index].type] : 'var(--bs-white)',
+                  borderColor: typeToColor[nodeType.outputs[index].type],
+                }}
                 className={data.isOutputConnected[index] ? 'connected' : ''}
                 isValidConnection={useConnectionValidator()}
               />
