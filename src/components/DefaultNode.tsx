31b5aec5b8abb93d881c4a8f0fdb545fd38e59da
SR150 <simonjrichards150@gmail.com>
2023-02-28 16:42:30 +0000

    fixed the isvalid connection checker

--- src/components/DefaultNode.tsx
+++ src/components/DefaultNode.tsx
@@ -20,6 +20,7 @@ function calcHandleTop(index: number, numHandles: number) {
 
 const useConnectionValidator = () => {
   const { getNode, getEdges } = useReactFlow();
+
   const nodeType = useRecoilValue(nodeTypesAtom);
   return useCallback(
     (connection: Connection) => {
@@ -34,11 +35,10 @@ const useConnectionValidator = () => {
         || connection.source === connection.target) return false;
       const target = getNode(connection.target);
       const edges = getConnectedEdges(target ? [target] : [], getEdges());
-      /* const inputType = nodeType[getNode(connection.target)?.data.nodeTypeId]
-        .inputs[connection.targetHandle].type;
-      const outputType = nodeType[getNode(connection.source)?.data.nodeTypeId]
-        .inputs[connection.sourceHandle].type;
-      if (inputType !== outputType) { return false; } */
+      const inputType = nodeType[getNode(connection.target)?.data.nodeTypeId].inputs[connection.targetHandle].type;
+      const outputType = nodeType[getNode(connection.source)?.data.nodeTypeId].outputs[connection.sourceHandle].type;
+
+      if (inputType !== outputType) { return false; }
       return edges.every((edge) =>
         !(edge.target === connection.target
           && edge.targetHandle === connection.targetHandle));
